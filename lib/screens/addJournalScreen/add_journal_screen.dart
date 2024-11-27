0a95d947eea085b7b4d8d01bafdf28815fc74f32
Nicolas Nascimento <nicolas.nascimento@serpro.gov.br>
2024-11-27 16:04:32 -0300

    criando parâmetro para dizer se é alteração ou não

--- lib/screens/addJournalScreen/add_journal_screen.dart
+++ lib/screens/addJournalScreen/add_journal_screen.dart
@@ -5,37 +5,49 @@ import 'package:flutter_webapi_first_course/services/journal_service.dart';
 
 class AddJournalScreen extends StatelessWidget {
   final Journal journal;
-  AddJournalScreen({super.key, required this.journal});
+  final bool isUpdate;
+  AddJournalScreen({super.key, required this.journal, required this.isUpdate});
   final TextEditingController _contentController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    _contentController.text = journal.content;
     return Scaffold(
       appBar: AppBar(
-        title: Text("${WeekDay(journal.createdAt.weekday).long.toLowerCase()}, ${journal.createdAt.day}  |  ${journal.createdAt.month}  |  ${journal.createdAt.year}",),
+        title: Text(
+          "${WeekDay(journal.createdAt.weekday).long.toLowerCase()}, ${journal.createdAt.day}  |  ${journal.createdAt.month}  |  ${journal.createdAt.year}",
+        ),
         actions: [
-        IconButton(
-          icon: const Icon(Icons.check),
-          onPressed: () {
-            JournalService service = JournalService();
-            journal.content = _contentController.text;
-            service.register(journal).then((value) {
-              Navigator.pop(context, value);
-            }); 
-            
-            
-          },
-        ),  
-      ],),
+          IconButton(
+            icon: const Icon(Icons.check),
+            onPressed: () {
+              journal.content = _contentController.text;
+              JournalService service = JournalService();
+              if (isUpdate == true) {
+                service.update(journal).then((value) {
+                  Navigator.pop(context, value);
+                });
+                return;
+              } else {
+                service.register(journal).then((value) {
+                Navigator.pop(context, value);
+                });
+              }
+            },
+          ),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: TextField(
           controller: _contentController,
           keyboardType: TextInputType.multiline,
-          style: TextStyle(fontSize: 24,),
+          style: TextStyle(
+            fontSize: 24,
+          ),
           expands: true,
           maxLines: null,
-          minLines: null ,
+          minLines: null,
         ),
       ),
     );
