876729a83569ece526b38de81df60c5ca70dc42f
Nicolas Nascimento <nicolas.nascimento@serpro.gov.br>
2024-11-27 11:59:48 -0300

    Refatoração na main, principal ongenerateRoute para passar argumentos pela rota nomeada ao ser chamada, passar o Journal neste caso

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webapi_first_course/services/journal_service.dart';
+import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/screens/addJournalScreen/add_journal_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
-
-  JournalService service = JournalService();
-  service.register("Olá Mundo!");
-  // service.get();
 }
 
 class MyApp extends StatelessWidget {
@@ -19,20 +16,31 @@ class MyApp extends StatelessWidget {
       title: 'Simple Journal',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        textTheme: GoogleFonts.bitterTextTheme(),
-        primarySwatch: Colors.grey,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.black, 
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-          ),
-        )
-      ),
+          textTheme: GoogleFonts.bitterTextTheme(),
+          primarySwatch: Colors.grey,
+          appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(color: Colors.white),
+            backgroundColor: Colors.black,
+            titleTextStyle: TextStyle(
+              color: Colors.white,
+            ),
+          )),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
       initialRoute: "home",
       routes: {
         "home": (context) => const HomeScreen(),
+        
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == "addJournal") {
+          return MaterialPageRoute(
+            builder: (context) => AddJournalScreen(
+              journal: settings.arguments as Journal,
+            ),
+          );
+        }
+        return null;
       },
     );
   }
