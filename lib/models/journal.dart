b454151281eef783f25ae034668333e53cd64124
Nicolas Nascimento <nicolas.nascimento@serpro.gov.br>
2024-11-27 11:58:52 -0300

    Refatoração do journal model, principal toMap para usar o json no banco

--- lib/models/journal.dart
+++ lib/models/journal.dart
@@ -1,3 +1,5 @@
+import 'package:uuid/uuid.dart';
+
 class Journal {
   String id;
   String content;
@@ -10,6 +12,31 @@ class Journal {
     required this.createdAt,
     required this.updatedAt,
   });
+
+  Journal.empty() :
+    id = Uuid().v1(),
+    content = "",
+    createdAt = DateTime.now(),
+    updatedAt = DateTime.now();
+  
+
+  factory Journal.fromJson(Map<String, dynamic> json) {
+    return Journal(
+      id: json["id"],
+      content: json["content"],
+      createdAt: DateTime.parse(json["created_at"]),
+      updatedAt: DateTime.parse(json["updated_at"]),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "content": content,
+      "created_at": createdAt.toIso8601String(),
+      "updated_at": updatedAt.toIso8601String(),
+    };
+  }
 
   @override
   String toString() {
